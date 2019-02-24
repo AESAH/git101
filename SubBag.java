1ebd4e4b40b0e46195169f2232382ce996b4e40e
AESAH <32261653+AESAH@users.noreply.github.com>
2019-02-25 00:33:38 +0700

    Update SubBag.java

--- SubBag.java
+++ SubBag.java
@@ -4,9 +4,7 @@ class SubBag extends Bag {
             System.out.println("IncreaseBlood: Hp+3");
         }else if(number==2){
             System.out.println("Attack: Exp+2 Hp-2");
-        }/*else if(number==3){
-            System.out.println("Stomp: Exp+4 Hp-2");
-        }*/
+        }
         
     }
 }
